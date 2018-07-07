071ce60bc26e4e3b2c1f00c03c9b1244f5d7d69f
srinigoriparthi <srini.goriparthi@gmail.com>
2018-07-07 13:10:57 -0700

    New Spring Boot API Website Project with Course Classes.

---
+++ course-api-website/src/main/java/io/javabrains/springbootquickstart/courseapiwebsite/CourseApiWebsiteApplication.java
@@ -0,0 +1,12 @@
+package io.javabrains.springbootquickstart.courseapiwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiWebsiteApplication.class, args);
+	}
+}
---
+++ course-api-website/src/test/java/io/javabrains/springbootquickstart/courseapiwebsite/CourseApiWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package io.javabrains.springbootquickstart.courseapiwebsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourseApiWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
